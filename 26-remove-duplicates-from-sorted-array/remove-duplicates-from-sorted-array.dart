97821c586e3e192059e66d91c3264874022185a1
edwin.joseph <edwin.joseph1258@gmail.com>
2025-12-09 10:55:14 +0530

    Time: 0 ms (100.00%) | Memory: 152.8 MB (6.64%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
@@ -0,0 +1,19 @@
+class Solution {
+  int removeDuplicates(List<int> nums) {
+    
+  if (nums.isEmpty) return 0;
+
+  int i = 1; 
+
+  for (int j = 1; j < nums.length; j++) {
+    if (nums[j] != nums[j - 1]) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+
+  return i; 
+}
+
+  
+}
